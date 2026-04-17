a0b9cdd588864cc465f7828e95e3570aa8b3218e
Nelson Khalil <nelsonkhalil111@gmail.com>
2026-04-17 15:13:11 +0200

    Particle and Power-Up Textures added and registered

--- core/src/main/java/io/github/nelsonkhalil/assetmanager/FileTexture.java
+++ core/src/main/java/io/github/nelsonkhalil/assetmanager/FileTexture.java
@@ -34,7 +34,48 @@ public enum FileTexture {
     NUM_6("numeral/numeral6.png"),
     NUM_7("numeral/numeral7.png"),
     NUM_8("numeral/numeral8.png"),
-    NUM_9("numeral/numeral9.png");
+    NUM_9("numeral/numeral9.png"),
+
+    BLACK_SMOKE_0("particle/black_smoke/blackSmoke00.png"),
+    BLACK_SMOKE_1("particle/black_smoke/blackSmoke01.png"),
+    BLACK_SMOKE_2("particle/black_smoke/blackSmoke02.png"),
+    BLACK_SMOKE_3("particle/black_smoke/blackSmoke03.png"),
+    BLACK_SMOKE_4("particle/black_smoke/blackSmoke04.png"),
+    BLACK_SMOKE_5("particle/black_smoke/blackSmoke05.png"),
+    BLACK_SMOKE_6("particle/black_smoke/blackSmoke06.png"),
+    BLACK_SMOKE_7("particle/black_smoke/blackSmoke07.png"),
+    BLACK_SMOKE_8("particle/black_smoke/blackSmoke08.png"),
+    BLACK_SMOKE_9("particle/black_smoke/blackSmoke09.png"),
+    BLACK_SMOKE_10("particle/black_smoke/blackSmoke10.png"),
+    BLACK_SMOKE_11("particle/black_smoke/blackSmoke11.png"),
+    BLACK_SMOKE_12("particle/black_smoke/blackSmoke12.png"),
+    BLACK_SMOKE_13("particle/black_smoke/blackSmoke13.png"),
+    BLACK_SMOKE_14("particle/black_smoke/blackSmoke14.png"),
+    BLACK_SMOKE_15("particle/black_smoke/blackSmoke15.png"),
+    BLACK_SMOKE_16("particle/black_smoke/blackSmoke16.png"),
+    BLACK_SMOKE_17("particle/black_smoke/blackSmoke17.png"),
+    BLACK_SMOKE_18("particle/black_smoke/blackSmoke18.png"),
+    BLACK_SMOKE_19("particle/black_smoke/blackSmoke19.png"),
+    BLACK_SMOKE_20("particle/black_smoke/blackSmoke20.png"),
+    BLACK_SMOKE_21("particle/black_smoke/blackSmoke21.png"),
+    BLACK_SMOKE_22("particle/black_smoke/blackSmoke22.png"),
+    BLACK_SMOKE_23("particle/black_smoke/blackSmoke23.png"),
+    BLACK_SMOKE_24("particle/black_smoke/blackSmoke24.png"),
+
+    EXPLOSION_0("particle/explosion/explosion00.png"),
+    EXPLOSION_1("particle/explosion/explosion01.png"),
+    EXPLOSION_2("particle/explosion/explosion02.png"),
+    EXPLOSION_3("particle/explosion/explosion03.png"),
+    EXPLOSION_4("particle/explosion/explosion04.png"),
+    EXPLOSION_5("particle/explosion/explosion05.png"),
+    EXPLOSION_6("particle/explosion/explosion06.png"),
+    EXPLOSION_7("particle/explosion/explosion07.png"),
+    EXPLOSION_8("particle/explosion/explosion08.png"),
+
+    POWER_UP_INVULNERABILITY("power_up/power_up_shield.png"),
+    POWER_UP_PLUS_ONE("power_up/power_up_star.png"),
+    POWER_UP_DOUBLE_SHOOT("power_up/power_up_bolt.png");
+
 
     public final String path;
     FileTexture(String path) {
